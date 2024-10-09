2f980dd0de63c47426bedaccb26c1ba750691015
Rena03528 <153510595+Rena03528@users.noreply.github.com>
2024-10-09 15:11:08 +0100

    Add files via upload

---
+++ x_matrix_creation.py
@@ -0,0 +1,27 @@
+def X_matrix_creation(n,m,d,train_u,train_y):
+
+    p = max(n, m + d)
+
+    N = len(train_u)
+
+    X_matrix = []
+
+    def create_phi_matrix(k, n, m):
+
+        y_theta = train_y[k-n:k]  
+        y_theta = y_theta[::-1]  
+
+        u_theta = train_u[k-d-m:k-d+1] 
+        u_theta = u_theta[::-1] 
+    
+        phi = np.concatenate((y_theta, u_theta))
+
+        return phi
+
+    for k in range(p, N):  
+        phi_matrix = create_phi_matrix(k, n, m)
+        X_matrix.append(phi_matrix) 
+
+    X_matrix = np.array(X_matrix)
+
+    return X_matrix
